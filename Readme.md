fa61fbf342b5597930a528632b5606126861a609
Paulius <pauliusp@fastmail.com>
2024-04-09 22:13:32 +0300

    Various improvements

--- Readme.md
+++ Readme.md
@@ -1,9 +1,4 @@
-
-
-
-
 ### Feature Engineering
-
 
 #### Feature Selection and Aggregation
 
@@ -12,13 +7,44 @@ Feature Tools DFS is used for feature engineering in combination with manual agg
 - bureau.csv
 - previous_application.csv
 - credit_card_balance.csv
+-
 
+#### Evaluation and Baseline
 
-#### Evaluation and Baseline 
+The upper range baseline (i.e. the best performing submission on Kaggle) is slightly over AUC = 0.8, we can't reasonably
+expect to beat it so our goal is to get as close as possible.
 
-The upper range baseline (i.e. the best performing submission on Kaggle) is slightly over AUC = 0.8, we can't reasonable expect to beat it so our goal is to get as close as possible.
+The lower range baseline is a simple classification model that's only using credit rating/score columns
+EXT_SOURCE_1, EXT_SOURCE_2, EXT_SOURCE_3. It achieves an AUC of ~ 0.72. This mean that the range between them is ralatively narrow which means that even a seemingly small increase in AUC e.g. by 0.01 would be pretty signficant. 
 
-Additionally, we've included a model selected using EvalML (and auto ML library) and a raw dataset (with Featuretools aggregations etc.)
+`_*An AUC of 0.5 suggests a model that performs no better than random guessing*_`
+
+Additionally, we've included a model selected using EvalML (and auto ML library) and a raw dataset (with Featuretools
+aggregations etc.)
 
 #### Clustering
 
+Clustering before using XGBoost can simplify data and possibly improve model performance by highlighting patterns that
+XGBoost may overlook. This preprocessing step reduces dimensionality and can enhance model interpretability, but its
+effectiveness depends on data relevance and feature importance evaluation.
+
+##### K-Prototypes
+
+Most suitable the dataset has clear boundaries and a roughly uniform distribution for optimal results. We've been unable
+to obtain clearly defined cluster when using it and based on the type of the dataset it's probably not the most suitable
+algorithm.
+
+#### DBSCAN
+
+Is an unsupervised algorithm which is more suitable for datasets with significant noise or irrelevant data points (e.g.
+data exhibits non-globular or irregularly shaped clusters)
+
+Feature
+
+- We've experimented
+
+#https://www.kaggle.com/c/home-credit-default-risk/data
+
+#### Tuning:
+- Bayesian optimization with Optuna
+- Multi objective optimization (i.e. for auc, pr-auc, f1, time) [TODO]
---
+++ src_draft/load_dataset.py
@@ -0,0 +1,84 @@
+import os
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def _internal_split_and_save_dfs(df: pd.DataFrame, base_output_dir: str, ds_key: str, no_split=False):
+    def save_parquet(df, subdir, filename):
+        output_dir = os.path.join(base_output_dir, subdir)
+        os.makedirs(output_dir, exist_ok=True)
+        filepath = os.path.join(output_dir, filename)
+        df.to_parquet(filepath)
+        print(f"Rows in {filepath}: {len(df)}")
+
+    def sample_split_save(df, sample_size, test_size, subdir):
+        if len(df) > sample_size:
+            df_sample = df.sample(n=sample_size, random_state=42)
+        else:
+            df_sample = df
+        # df_tuning, df_train_test = train_test_split(
+        #     df_sample, test_size=test_size, random_state=42
+        # )
+
+        # save_parquet(df_tuning, subdir, f"{ds_key}_tuning.parquet")
+        save_parquet(df_sample, subdir, f"{ds_key}.parquet")
+
+    # df_tuning, df_train_test = train_test_split(df, test_size=0.9, random_state=42)
+    save_parquet(df, "full", f"{ds_key}.parquet")
+
+    if not no_split:
+        sample_split_save(df, 100000, 0.9, "sample_100k")
+        print(len(df))
+        sample_split_save(df, 20000, 0.9, "sample_20k")
+
+
+def split_datasets_combined_accepted_rejected(
+        base_output_dir="../dataset",
+):
+    FILE_PATH_TRAIN = "../home-credit-default-risk/application_train.csv"
+    FILE_PATH_TEST = "../home-credit-default-risk/application_test.csv"
+
+    app_train_df = pd.read_csv(FILE_PATH_TRAIN)
+    app_test_df = pd.read_csv(FILE_PATH_TEST)
+
+    print(f"Total Lines in Source application_train: {len(app_train_df)}")
+    print(f"Total Lines in Source application_test: {len(app_test_df)}")
+
+    _internal_split_and_save_dfs(app_train_df, base_output_dir=base_output_dir, ds_key="app_train")
+    _internal_split_and_save_dfs(app_test_df, base_output_dir=base_output_dir, ds_key="app_test")
+
+    FILE_PATH_BUREU = "../home-credit-default-risk/bureau.csv"
+    bureau_df = pd.read_csv(FILE_PATH_BUREU)
+    _internal_split_and_save_dfs(bureau_df, base_output_dir=base_output_dir, ds_key="bureau", no_split=True)
+
+    FILE_PATH_BUREU = "../home-credit-default-risk/bureau_balance.csv"
+    bureau_df = pd.read_csv(FILE_PATH_BUREU)
+    _internal_split_and_save_dfs(bureau_df, base_output_dir=base_output_dir, ds_key="bureau_balance", no_split=True)
+
+    FILE_PATH_BUREU = "../home-credit-default-risk/credit_card_balance.csv"
+    bureau_df = pd.read_csv(FILE_PATH_BUREU)
+    _internal_split_and_save_dfs(bureau_df, base_output_dir=base_output_dir, ds_key="credit_card_balance",
+                                 no_split=True)
+
+    FILE_PATH_BUREU = "../home-credit-default-risk/installments_payments.csv"
+    bureau_df = pd.read_csv(FILE_PATH_BUREU)
+    _internal_split_and_save_dfs(bureau_df, base_output_dir=base_output_dir, ds_key="installments_payments",
+                                 no_split=True)
+
+    FILE_PATH_BUREU = "../home-credit-default-risk/previous_application.csv"
+    bureau_df = pd.read_csv(FILE_PATH_BUREU)
+    _internal_split_and_save_dfs(bureau_df, base_output_dir=base_output_dir, ds_key="previous_application",
+                                 no_split=True)
+
+    FILE_PATH_BUREU = "../home-credit-default-risk/POS_CASH_balance.csv"
+    bureau_df = pd.read_csv(FILE_PATH_BUREU)
+    _internal_split_and_save_dfs(bureau_df, base_output_dir=base_output_dir, ds_key="POS_CASH_balance", no_split=True)
+
+
+if __name__ == "__main__":
+    # split_and_save_datasets_for_accepted(
+    #     "../dataset/lending-club/accepted_2007_to_2018q4.csv/accepted_2007_to_2018Q4.csv"
+    # )
+    split_datasets_combined_accepted_rejected()
